820c26afe242e27ae63c8fb1835460fe38f42d94
Jo Nuckley <jnuckley@yahoo.co.uk>
2019-11-27 19:49:08 +0000

    made the setup_test_database.rb a method so that the spec helper will run the file for each test to truncate the table. Removed the single quotes and popped it in a method in the file itself

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,13 +1,21 @@
 require 'pg'
 
 class Bookmark
-
+=begin
+self allows calling the class and method from the controller without making a
+new instance everytime
+=end
   def self.all
+    #  if this is a test then it will connect to the test database as described in the spec_helper
     if ENV['ENVIRONMENT'] == 'test'
       connection = PG.connect(dbname: 'bookmark_manager_test')
     else
       connection = PG.connect(dbname: 'bookmark_manager')
     end
+=begin
+defining result as the response from selecting all from the bookmarks table
+once connected and then mapping the bookmarks to the word bookmark with the url
+=end
     result = connection.exec("SELECT * FROM bookmarks;")
     result.map {|bookmark| bookmark['url']}
   end
@@ -18,7 +26,10 @@ class Bookmark
   else
     connection = PG.connect(dbname: 'bookmark_manager')
   end
-
+=begin
+connected to the database, insert into the bookmarks table, url column,
+the url parameters from the table in the .erb. This is an SQL command.
+=end
   connection.exec("INSERT INTO bookmarks (url) VALUES('#{url}')")
 end
 
