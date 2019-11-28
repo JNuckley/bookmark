f246c5cd1480db7903fb6b29d5aef0cfdbbba016
Jo Nuckley <jnuckley@yahoo.co.uk>
2019-11-28 15:48:06 +0000

    removed the begin and end multiline comments as they were causing errors

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,10 +1,10 @@
 require 'pg'
 
 class Bookmark
-=begin
-self allows calling the class and method from the controller without making a
-new instance everytime
-=end
+
+  #  self allows calling the class and method from the controller without making a
+  #  new instance everytime
+
   def self.all
     #  if this is a test then it will connect to the test database as described in the spec_helper
     if ENV['ENVIRONMENT'] == 'test'
@@ -12,25 +12,24 @@ new instance everytime
     else
       connection = PG.connect(dbname: 'bookmark_manager')
     end
-=begin
-defining result as the response from selecting all from the bookmarks table
-once connected and then mapping the bookmarks to the word bookmark with the url
-=end
+
+    #  defining result as the response from selecting all from the bookmarks table
+    #  once connected and then mapping the bookmarks to the word bookmark with the url
+
     result = connection.exec("SELECT * FROM bookmarks;")
     result.map {|bookmark| bookmark['url']}
   end
 
   def self.create(url:)
-  if ENV['ENVIRONMENT'] == 'test'
-    connection = PG.connect(dbname: 'bookmark_manager_test')
-  else
-    connection = PG.connect(dbname: 'bookmark_manager')
-  end
-=begin
-connected to the database, insert into the bookmarks table, url column,
-the url parameters from the table in the .erb. This is an SQL command.
-=end
-  connection.exec("INSERT INTO bookmarks (url) VALUES('#{url}')")
-end
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+    else
+      connection = PG.connect(dbname: 'bookmark_manager')
+    end
 
+    #  connected to the database, insert into the bookmarks table, url column,
+    #  the url parameters from the table in the .erb. This is an SQL command.
+
+    connection.exec("INSERT INTO bookmarks (url) VALUES('#{url}')")
+  end
 end
---
+++ lib/post.rb
@@ -0,0 +1,11 @@
+# require 'pg'
+#
+# class Post
+#
+# def post_title
+# end
+#
+# def post_url
+# end
+#
+# end
